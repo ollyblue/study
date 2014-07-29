33ee198a56ee0bbfc22469cb3e61dac8f9f25cb9
ollyblue <ollybluew@gmail.com>
2014-07-29 17:04:42 +0800

    modify tpackage.py

--- python/tpackage.py
+++ python/tpackage.py
@@ -7,108 +7,155 @@ class tpack:
 	def __init__(self, endian = True):
 		self.s = list()
 		self.len = 0
-		'''little-endian or big-endian when, endian is True => big-endian'''
+		'''little-endian or big-endian when endian is True => big-endian'''
 		self.endian = endian
 
 	def get_len(self):
 		return self.len
 
 	def add_char(self, c):
-		pack =  ''
-		if self.endian == True:
-			pack = struct.pack('!c',c)
-		else:
-			pakc = struct.pack('c',c)
-		self.s.extend( list(pack))
+		fmt = "%sb" % ( self.endian == True and "!" or "")
+		pack = struct.pack(fmt, c)
+		self.s.extend(pack)
 		self.len += 1
 
 	def add_schar(self, c):
-		pack =  ''
 		fmt = "%sb" % ( self.endian == True and "!" or "")
-		print(fmt)
-		pakc = struct.pack(fmt,c)
-		self.s.extend( list(pack))
+		pack = struct.pack(fmt, c)
+		self.s.extend(pack)
 		self.len += 1
 
 	def add_uchar(self, c):
-		pack =  ''
-		if self.endian == True:
-			pack = struct.pack('!B',c)
-		else:
-			pakc = struct.pack('B',c)
-		self.s.extend( list(pack))
+		fmt = "%sB" % ( self.endian == True and "!" or "")
+		pack = struct.pack(fmt, c)
+		self.s.extend(pack)
 		self.len += 1
+
 	def add_short(self, v):
-		pack = ''
-		if self.endian == True:
-			pack = struct.pack('!h', v)
-		else:
-			pack = struct.pack('h', v)
-		self.s.extend(list(pack))
+		fmt = "%sh" % (self.endian == True and "!" or "")
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
 		self.len += 2
 
 	def add_ushort(self, v):
-		pass
+		fmt = "%sH" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 2
 
 	def add_int(self, v):
-		pass
+		fmt = "%si" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 4
+
 
 	def add_uint(self, v):
-		pass
+		fmt = "%sI" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 4
 
 	def add_long(self, v):
-		pass
+		fmt = "%sl" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 4
 
 	def add_ulong(self, v):
-		pass
+		fmt = "%sL" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 4
 
 	def add_longlong(self, v):
-		pack = ''
-		pack = struct.pack('q', v)
-		self.s.extend(list(pack))
+		fmt = "%sq" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
 		self.len += 8
 
 	def add_ulonglong(self, v):
-		pass
+		fmt = "%sQ" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 8
 
 	def add_float(self, v):
-		pass
+		fmt = "%sf" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 8
 
 	def add_double(self, v):
-		pass
+		fmt = "%sF" % ( self.endian == True and "!" or "" )
+		pack = struct.pack(fmt, v)
+		self.s.extend(pack)
+		self.len += 8
 
 	def add_cstr(self, len, s):
-		pass
+		fmt = "%sB%ds" % ( self.endian == True and "!" or "", len )
+		pack = struct.pack(fmt, len, s)
+		self.s.extend(pack)
+		self.len += 1
+		self.len += len
 
 	def add_wstr(self, len, s):
-		pass
+		fmt = "%sH%ds" % ( self.endian == True and "!" or "", len )
+		pack = struct.pack(fmt, len, s)
+		self.s.extend(pack)
+		self.len += 2
+		self.len += len
+
+	def add_dwstr(self, len, s):
+		fmt = "%sI%ds" % ( self.endian == True and "!" or "", len )
+		pack = struct.pack(fmt, len, s)
+		self.s.extend(pack)
+		self.len += 4
+		self.len += len
 		
 	def printf(self):
 		print(self.s)
 
 	def printhex(self):
-		self.len = len(self.s)
-		tlist = list(self.s)
-		#print(dir(self.s))
-	#	for x in self.s:
-			#print(dir(x))
-	#		print("%s" % x)
-		print("begin ascii")
+		print "begin ascii"
 		t_hex = binascii.b2a_hex(''.join(self.s))
-		print("len t_hex:%d" % len(t_hex))
+		len_hex = len(t_hex)
+		hex_line = 0
+		hex_num = 0
+
+		for x in xrange(0, len_hex, 4):
+			if( x == 0 or x % 32 == 0 ):
+				print ""
+				print "%06X\t" % (hex_line/8),
+			print t_hex[x:x+4],
+			hex_line += 1
+
+		'''
+			if( len_hex % 2 == 0 and len_hex == 0 ):
+				print "%s " %t_hex[x:x+2],
+				len_hex += 1
+			else:
+				print "%s" %t_hex[x:x+2],
+				len_hex += 1
+		'''
+
+		print("\nlen t_hex:%d" % len(t_hex))
 		print(binascii.b2a_hex(''.join(self.s)))
 		print("end ascii")
-		for i in xrange(0,self.len):
-			print("i:%d v:%s" % (i, tlist[i]))
 
 
 if __name__ == "__main__":
 	t = tpack()
-	t.add_char("A")
+	#t.add_char("A")
 	#t.add_schar(127)
 	#t.add_schar(15)
-	t.add_short(32767)
-	t.add_longlong(12345678)
+	#t.add_short(32767)
+	#t.add_longlong(12345678)
+	s = "test"
+
+	t.add_ushort(0x11FF)
+	t.add_ulonglong(0x11FF)
+	t.add_wstr(len(s), s)
 	print("len:%d" % t.get_len())
 	#t.printf()
 	t.printhex()
